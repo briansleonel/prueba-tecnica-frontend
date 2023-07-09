f4357a3387d43aedd6b7cd3d06afbb725b8a712b
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-07-09 17:07:30 -0300

    feat: add logic to select question and advance to next question

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,60 @@
+import { useState, useEffect } from "react";
+import data from "./data/data.json";
+import { IQuestion } from "./types/question";
+
 function App() {
-    return <>Prueba técnica frontend</>;
+    const [indexQuestion, setIndexQuestion] = useState(0);
+    const [question, setQuestion] = useState<IQuestion>(data[indexQuestion]);
+    const [selectedAnswer, setSelectedAnswer] = useState(false);
+
+    const onSelectAnswer = (selected: string) => {
+        if (!selectedAnswer) {
+            setSelectedAnswer(true);
+            console.log(selected);
+            nextQuestion();
+        }
+    };
+
+    const nextQuestion = () => {
+        const newIndex = indexQuestion + 1;
+        if (newIndex < data.length) setIndexQuestion(indexQuestion + 1);
+        else console.log("Finished");
+    };
+
+    useEffect(() => {
+        setQuestion(data[indexQuestion]);
+        setSelectedAnswer(false);
+    }, [indexQuestion]);
+
+    return (
+        <section>
+            <div>{question.question}</div>
+            <button
+                type="button"
+                onClick={() => onSelectAnswer(question.options.a)}
+            >
+                {question.options.a}
+            </button>
+            <button
+                type="button"
+                onClick={() => onSelectAnswer(question.options.b)}
+            >
+                {question.options.b}
+            </button>
+            <button
+                type="button"
+                onClick={() => onSelectAnswer(question.options.c)}
+            >
+                {question.options.c}
+            </button>
+            <button
+                type="button"
+                onClick={() => onSelectAnswer(question.options.d)}
+            >
+                {question.options.d}
+            </button>
+        </section>
+    );
 }
 
 export default App;
